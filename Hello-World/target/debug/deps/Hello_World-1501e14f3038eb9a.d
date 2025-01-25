645a530f88fe4091f0aae66fcae071e4ef8120c2
Lingxiao Han <hanlingx@unc.edu>
2025-01-25 04:48:22 +0000

    Finished rust Hello World program

---
+++ Hello-World/target/debug/deps/Hello_World-1501e14f3038eb9a.d
@@ -0,0 +1,5 @@
+/workspaces/rust-language-tutorial/Hello-World/target/debug/deps/libHello_World-1501e14f3038eb9a.rmeta: src/main.rs
+
+/workspaces/rust-language-tutorial/Hello-World/target/debug/deps/Hello_World-1501e14f3038eb9a.d: src/main.rs
+
+src/main.rs:
---
+++ Hello-World/target/debug/deps/Hello_World-fb55513fd580cb86.d
@@ -0,0 +1,5 @@
+/workspaces/rust-language-tutorial/Hello-World/target/debug/deps/libHello_World-fb55513fd580cb86.rmeta: src/main.rs
+
+/workspaces/rust-language-tutorial/Hello-World/target/debug/deps/Hello_World-fb55513fd580cb86.d: src/main.rs
+
+src/main.rs:
